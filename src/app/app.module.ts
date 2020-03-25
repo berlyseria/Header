a88b1aa13d92b87dbfdcb0d6ac8d20f891aad616
Kimberly Seria <kseria@gms.ca>
2020-03-25 12:09:08 -0600

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [HeaderComponent]
+  // bootstrap: [HeaderComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector) { }
+
+  ngDoBootstrap() {
+    const el = createCustomElement(HeaderComponent, { injector: this.injector });
+    customElements.define('header-element', el);
+  }
+}
