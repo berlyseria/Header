cc3abfc1ebfccafc69f2de439d8cdf8f3871fd5b
Kimberly Seria <kseria@gms.ca>
2020-03-27 13:50:50 -0600

    added shadowDOM

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class HeaderComponent {
   title = 'header';
